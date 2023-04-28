0d5c34edf41e651b2634200f3226d690c77bb3c0
AUCL051098 <89956282+AUCL051098@users.noreply.github.com>
2023-04-27 22:53:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <main class="container p-4">
   <div class="row">
     <div class="col-md-4">
-      <!-- MESSAGES -->
+      
 
       <?php if (isset($_SESSION['message'])) { ?>
       <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
@@ -16,16 +16,16 @@
       </div>
       <?php session_unset(); } ?>
 
-      <!-- ADD TASK FORM -->
+      
       <div class="card card-body">
         <form action="save_task.php" method="POST">
           <div class="form-group">
-            <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
+            <input type="text" name="title" class="form-control" placeholder="Nombre del empleado" autofocus>
           </div>
           <div class="form-group">
-            <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
+            <textarea name="description" rows="2" class="form-control" placeholder="Area/Numero de empleado"></textarea>
           </div>
-          <input type="submit" name="save_task" class="btn btn-success btn-block" value="Save Task">
+          <input type="submit" name="save_task" class="btn btn-success btn-block" value="Guardar">
         </form>
       </div>
     </div>
@@ -33,10 +33,10 @@
       <table class="table table-bordered">
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Created At</th>
-            <th>Action</th>
+            <th>Nombre Empleado</th>
+            <th>Área/Numero de empleado</th>
+            <th>Ingreso</th>
+            <th>Accion</th>
           </tr>
         </thead>
         <tbody>
