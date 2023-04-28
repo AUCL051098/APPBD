a82c22114330279ffe1cd7fa374a906b79e07706
AUCL051098 <89956282+AUCL051098@users.noreply.github.com>
2023-04-27 23:37:35 -0600

    Update save_task.php

--- save_task.php
+++ save_task.php
@@ -11,7 +11,7 @@ if (isset($_POST['save_task'])) {
     die("Query Failed.");
   }
 
-  $_SESSION['message'] = 'Task Saved Successfully';
+  $_SESSION['message'] = 'El registro se guardó exitosamente';
   $_SESSION['message_type'] = 'success';
   header('Location: index.php');
 
